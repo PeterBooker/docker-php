de4be510f49206f37dd0f986d612d1099d31604b
Peter Booker <mail@peterbooker.com>
2019-07-13 21:39:49 +0100

    Add wordpress image

---
+++ wp/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /var/www/html
+if $(wp core is-installed); then
+    wp core download --version=${WP_VERSION:-latest} --force --allow-root \
+    && wp core config --dbname=${MYSQL_DATABASE:-wordpress} --dbuser=${MYSQL_USER:-wordpress} --dbpass=${MYSQL_PASSWORD:-password} --dbhost=database --force --allow-root \
+    && wp core install --url=localhost --title=WordPress --admin_user=${WP_USER:-user} --admin_password=${WP_PASS:-password} --admin_email=${WP_EMAIL:-user@domain.com} --allow-root \
+    && wp rewrite structure "/%postname%/" --allow-root \
+    && wp site empty --allow-root \
+    && wp widget delete search-2 recent-posts-2 recent-comments-2 archives-2 categories-2 meta-2 --allow-root
+fi
+
+exec "$@"
