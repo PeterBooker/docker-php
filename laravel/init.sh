e52451f0ffa4492923dab9c9af64622eb308bd2b
Peter Booker <mail@peterbooker.com>
2019-07-13 21:39:40 +0100

    Add laravel image

---
+++ laravel/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /var/www/adminer
+
+if [[ ! -e adminer.php ]]; then
+    wget https://github.com/vrana/adminer/releases/download/v4.7.1/adminer-4.7.1.php -O index.php
+fi
+
+exec "$@"
